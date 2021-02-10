a7d1064fba7a569764a57f973b192532f8c83a19
Jerry <calljerryli@outlook.com>
2021-02-10 14:08:34 +0800

    feat: Fund component

---
+++ src/component/Fund.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+const Fund = (props) => {
+  const [jerryIndex, setJerryIndex] = useState(0)
+  const [jerryIndexClassName, setJerryIndexClassName] = useState('info')
+
+  const fetchJerryIndex = () => {
+    const url = `http://127.0.0.1:8080/fund/jerryIndex/fundCode/${props.code}`
+    axios.get(url).then(response => {
+      console.log(response)
+
+      const jerryIndex = response.data
+      setJerryIndex(jerryIndex)
+
+      if (jerryIndex <= -2) {
+        setJerryIndexClassName('success')
+      } else if (jerryIndex >= 5) {
+        setJerryIndexClassName('danger')
+      } else if (jerryIndex >= 2) {
+        setJerryIndexClassName('warning')
+      } else {
+        setJerryIndexClassName('info')
+      }
+    }).catch(e => {
+      console.error(e)
+    })
+  }
+
+  useEffect(() => {
+    fetchJerryIndex()
+  }, [jerryIndex])
+
+  return (
+    <div>
+      <table>
+        <thead>
+        <th>
+          <td>名称</td>
+        </th>
+        <th>
+          <td>代码</td>
+        </th>
+        <th>
+          <td>投资指标</td>
+        </th>
+        <th>
+          <td>更新指标</td>
+        </th>
+        </thead>
+        <tbody>
+        <tr>
+          <td>{props.name}</td>
+          <td>{props.code}</td>
+          <td className={jerryIndexClassName}>{jerryIndex}</td>
+          <td>
+            <button onClick={fetchJerryIndex}>更新指标</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Fund
