81bfafd98ad1435a3061f4757ad0b860da755c63
Jerry <calljerryli@outlook.com>
2021-02-12 23:07:36 +0800

    chore: update icon

--- src/component/Fund.js
+++ src/component/Fund.js
@@ -51,13 +51,48 @@ const Fund = (props) => {
       <td>{props.code}</td>
       <td className={jerryIndexClassName}>{jerryIndex}</td>
       <td>
-        <button onClick={fetchJerryIndex}>更新指标</button>
+        <button onClick={fetchJerryIndex}>
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              d="M1.75 16.002C3.353 20.098 7.338 23 12 23c6.075 0 11-4.925 11-11m-.75-4.002C20.649 3.901 16.663 1 12 1C5.925 1 1 5.925 1 12m8 4H1v8M23 0v8h-8"
+            />
+          </svg>
+        </button>
       </td>
       <td>
-        <button onClick={handleRemoveBtnClick}>删除</button>
+        <button onClick={handleRemoveBtnClick}>
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6v12z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
       </td>
       <td>
-        <button onClick={handleDetailBtnClick}>查看详情</button>
+        <button onClick={handleDetailBtnClick}>
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M10.59 13.41c.41.39.41 1.03 0 1.42c-.39.39-1.03.39-1.42 0a5.003 5.003 0 0 1 0-7.07l3.54-3.54a5.003 5.003 0 0 1 7.07 0a5.003 5.003 0 0 1 0 7.07l-1.49 1.49c.01-.82-.12-1.64-.4-2.42l.47-.48a2.982 2.982 0 0 0 0-4.24a2.982 2.982 0 0 0-4.24 0l-3.53 3.53a2.982 2.982 0 0 0 0 4.24m2.82-4.24c.39-.39 1.03-.39 1.42 0a5.003 5.003 0 0 1 0 7.07l-3.54 3.54a5.003 5.003 0 0 1-7.07 0a5.003 5.003 0 0 1 0-7.07l1.49-1.49c-.01.82.12 1.64.4 2.43l-.47.47a2.982 2.982 0 0 0 0 4.24a2.982 2.982 0 0 0 4.24 0l3.53-3.53a2.982 2.982 0 0 0 0-4.24a.973.973 0 0 1 0-1.42z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
       </td>
     </tr>
   )
