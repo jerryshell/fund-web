ecb7d38679c4649be6df9dadc919d9b3bd5e2eb1
Jerry <calljerryli@outlook.com>
2021-06-15 15:04:47 +0800

    chore: update fundReducer initState

--- src/redux/fundReducer.js
+++ src/redux/fundReducer.js
@@ -2,98 +2,23 @@ import { actionType } from './actionType'
 
 const initState = [
   {
-    'name': '景顺长城新兴成长混合',
-    'code': '260108',
+    'name': '华夏沪深300ETF联接A',
+    'code': '000051',
     'jerryIndex': 0,
   },
   {
-    'name': '交银定期支付双息平衡混合',
-    'code': '519732',
+    'name': '天弘沪深300ETF联接A',
+    'code': '000961',
     'jerryIndex': 0,
   },
   {
-    'name': '易方达蓝筹精选混合',
-    'code': '005827',
+    'name': '华宝中证100指数A',
+    'code': '240014',
     'jerryIndex': 0,
   },
   {
-    'name': '工银瑞信文体产业股票A',
-    'code': '001714',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '易方达中小盘混合',
-    'code': '110011',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '泓德远见回报混合',
-    'code': '001500',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '兴全合润混合',
-    'code': '163406',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '兴全趋势投资混合(LOF)',
-    'code': '163402',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '广发稳健增长A',
-    'code': '270002',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '中欧时代先锋股票C',
-    'code': '004241',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '富国新动力C',
-    'code': '001510',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '国富沪港深成长精选股票',
-    'code': '001605',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '南方优选成长混合',
-    'code': '202023',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '富国沪港深价值混合A',
-    'code': '001371',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '景顺长城优选混合',
-    'code': '260101',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '银华中小盘混合',
-    'code': '180031',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '中欧新蓝筹混合C',
-    'code': '004237',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '东方红产业升级混合',
-    'code': '000619',
-    'jerryIndex': 0,
-  },
-  {
-    'name': '鹏华盛世创新混合(LOF)',
-    'code': '160613',
+    'name': '交银创业板50指数A',
+    'code': '007464',
     'jerryIndex': 0,
   },
 ]
